82164e3777526621469309327fe559d1cd410fb3
bluskar <along.hexo@gmail.com>
2024-09-27 23:07:42 +0800

    test comit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <p id="index-page">
-      This is a demo for Remix. hi;
+      This is a demo for Remix. hi
       <br />
       Check out{" "}
       <a href="https://remix.run">the docs at remix.run</a>.
